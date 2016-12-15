8550ed17151b7b6e8d203be8a7799e0a56789fc2
Michael Price <mike@mightystrongmedia.com>
2016-12-15 13:19:14 -0700

    Added jqueryMatchHeight and scrollTo

--- app/assets/js/prettydocs.js
+++ app/assets/js/prettydocs.js
@@ -1,2 +1,4 @@
 //= require bootstrap-sprockets
+//= require jquery-match-height/jquery.matchHeight
+//= require jquery-scrollTo/jquery.scrollTo
 //= require main
--- lib/prettydocs_rails.rb
+++ lib/prettydocs_rails.rb
@@ -2,6 +2,7 @@ require 'prettydocs_rails/version'
 require 'prettydocs_rails/engine'
 require 'bootstrap-sass'
 require 'font-awesome-rails'
+require 'prism-rails'
 
 module PrettydocsRails
 end
