f18a38755e5a3e6b43ac3f5560837a04c3ad47db
Michael Price <mike@mightystrongmedia.com>
2016-12-16 09:02:38 -0700

    Precomile assets.

--- lib/prettydocs_rails/engine.rb
+++ lib/prettydocs_rails/engine.rb
@@ -1,4 +1,10 @@
 module PrettydocsRails
   class Engine < ::Rails::Engine
+    initializer 'prettydocs_rails.assets.precompile' do |app|
+      %w(stylesheets javascripts fonts images).each do |sub|
+        app.config.assets.paths << root.join('assets', sub).to_s
+        app.config.assets.paths << root.join('vendor', 'assets', sub).to_s
+      end
+    end
   end
 end
