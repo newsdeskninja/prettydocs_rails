babae171aee4b21c85d32442ce57917461c90fde
Michael Price <mike@mightystrongmedia.com>
2016-12-16 09:43:48 -0700

    Added runtime dependencies and switch to depend on railties instead of rails.

--- lib/prettydocs_rails.rb
+++ lib/prettydocs_rails.rb
@@ -1,7 +1,7 @@
-require 'prettydocs_rails/version'
-require 'prettydocs_rails/engine'
 require 'bootstrap-sass'
 require 'font-awesome-rails'
+require 'prettydocs_rails/engine'
+require 'prettydocs_rails/version'
 
 module PrettydocsRails
   module Rails
