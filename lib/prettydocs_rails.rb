2ed20a71a93a54f8585803840dfa6cec42da3256
Michael Price <mike@mightystrongmedia.com>
2016-12-20 11:04:46 -0700

    Attempt to register stylesheets.

--- lib/prettydocs_rails.rb
+++ lib/prettydocs_rails.rb
@@ -4,6 +4,21 @@ require 'prettydocs_rails/engine'
 require 'prettydocs_rails/version'
 
 module PrettydocsRails
-  module Rails
+  class << self
+
+    def load!
+      configure_sass
+    end
+
+    def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+
+    private
+      def configure_sass
+        require 'sass'
+
+        Bootstrap::Sass.load_paths << stylesheets_path
+      end
   end
 end
--- lib/prettydocs_rails/engine.rb
+++ lib/prettydocs_rails/engine.rb
@@ -1,11 +1,9 @@
 module PrettydocsRails
-  module Rails
-    class Engine < ::Rails::Engine
-      initializer 'prettydocs_rails.assets.precompile' do |app|
-        %w(stylesheets javascripts fonts images).each do |sub|
-          app.config.assets.paths << root.join('assets', sub).to_s
-          app.config.assets.paths << root.join('vendor', 'assets', sub).to_s
-        end
+  class Engine < ::Rails::Engine
+    initializer 'prettydocs_rails.assets.precompile' do |app|
+      %w(stylesheets javascripts fonts images).each do |sub|
+        app.config.assets.paths << root.join('assets', sub).to_s
+        app.config.assets.paths << root.join('vendor', 'assets', sub).to_s
       end
     end
   end
